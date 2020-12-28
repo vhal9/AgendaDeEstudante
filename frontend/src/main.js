986a78412da408abed5c4fbdf0364de5943272b8
victorhugo <victor.landin@estudante.ufla.br>
2020-12-28 18:42:28 -0300

    importado o vue-validate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import vuetify from './plugins/vuetify';
-
 import VueToast from 'vue-toast-notification';
 
+import { ValidationProvider } from 'vee-validate';
+
+Vue.component('ValidationProvider', ValidationProvider);
 
 Vue.use(VueToast, {
   duration: 2000,
