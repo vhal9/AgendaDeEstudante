9d7da5aab378544a9efefb03bed28c286587ca12
victorhugo <victor.landin@estudante.ufla.br>
2020-12-29 15:07:37 -0300

    reformulação da tela de cadastro de etiqueta

--- frontend/src/views/CadastroEtiqueta.vue
+++ frontend/src/views/CadastroEtiqueta.vue
@@ -1,102 +1,80 @@
-<template>
-    <div class="container">
-        <div class="titulo">
-            <h2>Cadastrar Etiqueta</h2>
-        </div>
-        <div class="form">
-            <div class="col-md-5" >
-                <b-form-input v-model="etiqueta.nome" placeholder="nome"></b-form-input>
-            </div>
-              
-        </div>
-      
-      <div class="botoes">
-          <div class="col-md-1">
-              <b-button variant="warning" class="link" to="/"> Voltar </b-button>
-          </div>
-          <div class="col-md-1">
-              <b-button variant="warning" @click="salvarEtiqueta" :disabled='isDisabled'> Cadastrar </b-button>
-          </div>
-      </div>
-    </div>
+<template lang="pug">
+	#formulario.container
+		h3 Cadastrar Etiqueta</h2>
+		v-form(ref='form', lazy-validation)
+			v-text-field(v-model="etiqueta.nome" label="Nome" :rules='nomeRule')
+			Botao(label="Limpar", @clickBotao='limpar')
+			Botao(label="Salvar", :disabled="isDisabled", @clickBotao='salvarEtiqueta')
+
 </template>
 
 <script>
-    import EtiquetaService from '../services/etiquetaService';
-    export default {
-        name: 'CadastroEtiqueta',
-        data(){
-            return {
-                etiqueta:{
-                    id:null,
-                    nome:'',
-                },
-            }
-        },
-        computed:{
-            isDisabled: function(){
-                return this.desabilitarBotaoSalvar();
-            },
-        },
-        methods:{
-            salvarEtiqueta(){
-                EtiquetaService.salvar(this.etiqueta)
-                .then(resposta => {
-                    if(resposta.status === 201){
-                        this.$toast.success(resposta.data);
-                        this.limpar();
-                    }
-                    
-                }).catch(error => {
-                    this.$toast.error(error);
-                });
-            },
-            desabilitarBotaoSalvar(){
-                if(this.etiqueta.nome === "") return true;
-            },
-            limpar(){
-                this.etiqueta.id = null;
-                this.etiqueta.nome = ''
-            },
-            verificarLogin(){
-                if(localStorage.nome === undefined || localStorage.nome === null){
-                    this.$router.push("/login")
-                }
-            }
+	import Botao from '../components/atoms/Botoes';
+	import EtiquetaService from '../services/etiquetaService';
+	export default {
+		name: 'CadastroEtiqueta',
+		components:{
+			Botao,
+		},
+		data(){
+			return {
+				etiqueta:{
+					id:null,
+					nome:'',
+				},
+				nomeRule:[
+					v => !!v || 'Nome é obrigátorio',
+					v => (v && v.length >= 3 && v.length <= 10  ) || 'Nome deve conter entre 3 e 10 caracteres'  
+				],
+			}
+		},
+		computed:{
+			isDisabled: function(){
+				return this.desabilitarBotaoSalvar();
+			},
+		},
+		methods:{
+			salvarEtiqueta(){
+				EtiquetaService.salvar(this.etiqueta)
+				.then(resposta => {
+					if(resposta.status === 201){
+						this.$toast.success(resposta.data);
+						this.limpar();
+					}
+					
+				}).catch(error => {
+					this.$toast.error(error);
+				});
+			},
+			desabilitarBotaoSalvar(){
+				if(this.etiqueta.nome === "") return true;
+				if(this.etiqueta.nome.length < 3 || this.etiqueta.nome.length > 10) return true;
+				return false; 
+			},
+			limpar(){
+				this.$refs.form.reset()
+				this.etiqueta.id = null;
+				this.etiqueta.nome = ''
+			},
+			verificarLogin(){
+				if(localStorage.nome === undefined || localStorage.nome === null){
+					this.$router.push("/login")
+				}
+			}
 
-        },
-        mounted(){
-            this.verificarLogin();
-        }
+		},
+		mounted(){
+			this.verificarLogin();
+		}
 
-    }
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .container{
-        background-color: #ffff99;
-        margin-top: 5%;
-        min-height: 300px;
-        align-items: center;
-    }
-    .titulo{
-        margin-top: 2%;
-        padding: 10px;
-    }
-    .form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 2%;
-    }
-    .botoes{
-        display: flex;
-        flex-direction: row;
-        justify-content:center;
-        margin-top: 2%;
-    }
-    .link{
-        color:black;
-    }
+	#formulario{
+		margin-top: 10%;
+		font-size: 1.3rem;
+		text-align: center;
+	}
 </style>
