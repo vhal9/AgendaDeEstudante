341834181f45d892555428decae9141eb5e5b160
victorhugo <victor.landin@estudante.ufla.br>
2020-12-29 15:17:46 -0300

    desabilitar botão salvar quando os textos não atenderam a restrição de tamanho e troca de identação para tabs

--- frontend/src/views/CadastroTarefa.vue
+++ frontend/src/views/CadastroTarefa.vue
@@ -13,14 +13,14 @@
 </template>
 
 <script>
-import EtiquetaService from '../services/etiquetaService';
-import TarefaService from '../services/tarefasService';
-import Botao from '../components/atoms/Botoes';
+	import EtiquetaService from '../services/etiquetaService';
+	import TarefaService from '../services/tarefasService';
+	import Botao from '../components/atoms/Botoes';
 
-const clone = require("lodash.clone");
+	const clone = require("lodash.clone");
 	export default {
-    name: 'CadastroTarefa',
-    components:{
+	name: 'CadastroTarefa',
+	components:{
 		Botao
 	},
 	data(){
@@ -36,10 +36,9 @@ const clone = require("lodash.clone");
 				}
 			},
 			etiquetas:[{ value: null, text: 'Etiqueta' },],
-			msg:'',
 			tituloRule:[
 				v => !!v || 'Título é obrigátorio',
-                v => (v && v.length >= 3 && v.length <= 20  ) || 'Título deve conter entre 3 e 20 caracteres'  
+				v => (v && v.length >= 3 && v.length <= 20  ) || 'Título deve conter entre 3 e 20 caracteres'  
 			],
 			descricaoRule:[
 				v => !!v || 'Descrição é obrigátoria',
@@ -53,72 +52,70 @@ const clone = require("lodash.clone");
 			]
 		}
 	},
-    computed:{
-        isDisabled: function(){
-            return this.desabilitarBotaoSalvar();
-        },
-    },
-    methods:{
-        salvarTarefa(){
-            let tarefaSalvar = clone(this.tarefa);
-            tarefaSalvar.etiqueta = clone({'id': this.tarefa.etiqueta})
-            TarefaService.salvar(tarefaSalvar).then(resposta => {
-                if(resposta.status === 201){
-                    this.$toast.success(resposta.data);
-                    this.limpar();
-                }
-                
-            }).catch(error => {
-                this.$toast.error(error);
-            });
-        },
-        desabilitarBotaoSalvar(){
-            if(this.tarefa.titulo === "") return true;
-            if(this.tarefa.descricao === "") return true;
-            if(this.tarefa.data === "") return true;
-            if(this.tarefa.etiqueta === null) return true;
+	computed:{
+		isDisabled: function(){
+			return this.desabilitarBotaoSalvar();
+		},
+	},
+	methods:{
+		salvarTarefa(){
+			let tarefaSalvar = clone(this.tarefa);
+			tarefaSalvar.etiqueta = clone({'id': this.tarefa.etiqueta})
+			TarefaService.salvar(tarefaSalvar).then(resposta => {
+				if(resposta.status === 201){
+					this.$toast.success(resposta.data);
+					this.limpar();
+				}
+				
+			}).catch(error => {
+				this.$toast.error(error);
+			});
+		},
+		desabilitarBotaoSalvar(){
+			if(this.tarefa.titulo === "" || this.tarefa.titulo.length < 3 || this.tarefa.titulo.length > 20 ) return true;
+			if(this.tarefa.descricao === "" || this.tarefa.descricao.length < 5 || this.tarefa.descricao > 30) return true;
+			if(this.tarefa.data === "") return true;
+			if(this.tarefa.etiqueta === null) return true;
 			return false;
 
-        },
-        listarEtiquetas(){
-            EtiquetaService.listar().then(resposta =>{
-                let value;
-                let text;
-                resposta.data.forEach(element => {
-                    value = element.id;
-                    text = element.nome;
-                    this.etiquetas.push({'value': value, 'text':text});
-                }); 
-            })
-        },
-        limpar(){
-            console.log('teste2')
+		},
+		listarEtiquetas(){
+			EtiquetaService.listar().then(resposta =>{
+				let value;
+				let text;
+				resposta.data.forEach(element => {
+					value = element.id;
+					text = element.nome;
+					this.etiquetas.push({'value': value, 'text':text});
+				}); 
+			})
+		},
+		limpar(){
 			this.$refs.form.reset()
-            this.tarefa ={
-                id:null,
-                titulo:'',
-                descricao:'',
-                data:'',
-                etiqueta:null,
-                usuario:{
-                    id: null
-                }
+			this.tarefa ={
+				id:null,
+				titulo:'',
+				descricao:'',
+				data:'',
+				etiqueta:null,
+				usuario:{
+					id: null
+				}
 			}
 			
-        },
-        verificarLogin(){
-            if(localStorage.nome === undefined || localStorage.nome === null){
-                this.$router.push("/login")
-            }
-        }
-        
+		},
+		verificarLogin(){
+			if(localStorage.nome === undefined || localStorage.nome === null){
+				this.$router.push("/login")
+			}
+		}
+		
 
-    },
-    mounted(){
+	},
+	mounted(){
 		this.verificarLogin();
-        this.listarEtiquetas();
-    },
-  
+		this.listarEtiquetas();
+	},
 }
 </script>
 
@@ -126,7 +123,7 @@ const clone = require("lodash.clone");
 <style scoped>
 	#formulario{
 		margin-top: 10%;
-        font-size: 1.3rem;
-        text-align: center;
+		font-size: 1.3rem;
+		text-align: center;
 	}
 </style>
